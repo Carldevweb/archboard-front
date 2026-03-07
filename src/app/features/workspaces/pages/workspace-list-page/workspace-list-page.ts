740bfe9f22098b82db20097c408ad753ec27d0f4
Carldevweb <carlflahault@gmail.com>
2026-03-07 09:12:52 +0100

    Initial commit - ArchBoard Kanban frontend (Angular 21 + SCSS setup)

---
+++ src/app/features/workspaces/pages/workspace-list-page/workspace-list-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workspace-list-page',
+  imports: [],
+  templateUrl: './workspace-list-page.html',
+  styleUrl: './workspace-list-page.scss',
+})
+export class WorkspaceListPage {}
